c402a38f944bf812c563a0a4415a631a8fe9f2e5
Daan Seegers <d.seegers.mail@gmail.com>
2019-06-26 20:09:46 +0200

    add some functions

--- spam-is-bad.php
+++ spam-is-bad.php
@@ -1,13 +1,18 @@
 <?php
-/**
-* Plugin Name: Spam is bad
-* Plugin URI: 
-* Description: Whoop whoop block that spam
-* Version: 1.0
-* Author: Your Name Here
-* Author URI: https://github.com/dseegers
-**/
+//Silence is golden
+require('vendor/autoload.php');
 
+/**
+ * Plugin Name: Spam is bad
+ * Plugin URI:
+ * Description: Whoop whoop block that spam
+ * Version: 1.0
+ * Author: Your Name Here
+ * Author URI: https://github.com/dseegers
+ **/
 
 new Flx\NoSpam\Service();
+
+
+
 
