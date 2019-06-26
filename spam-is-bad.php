d6e5fc991ed6265ad7ad6516c4b997f8022b0b73
Daan Seegers <d.seegers.mail@gmail.com>
2019-06-26 16:36:21 +0200

    Create spam-is-bad.php

---
+++ spam-is-bad.php
@@ -0,0 +1,9 @@
+<?php
+/**
+* Plugin Name: Spam is bad
+* Plugin URI: 
+* Description: Whoop whoop block that spam
+* Version: 1.0
+* Author: Your Name Here
+* Author URI: https://github.com/dseegers
+**/
