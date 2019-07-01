ed0040b671012dcd3d1c2a5dce7a6e54252ae152
Daan Seegers <daan@raow.nl>
2019-07-01 10:29:56 +0200

    company_tax_id

--- src/Service.php
+++ src/Service.php
@@ -76,11 +76,16 @@ class Service
         register_setting("header_section", "company_email");
 
 //        Company slogan
-        add_settings_field("company_slogan", "Company Email", [$this, "company_slogan_form_element"], "seo-options",
+        add_settings_field("company_slogan", "Company Slogan", [$this, "company_slogan_form_element"], "seo-options",
             "header_section");
         register_setting("header_section", "company_slogan");
 
         //Company tax id
+        add_settings_field("company_tax_id", "Company Tax Id", [$this, "company_tax_id_form_element"], "seo-options",
+            "header_section");
+        register_setting("header_section", "company_tax_id");
+
+
         //Company price class
         //Company type
 //Contact details
@@ -123,6 +128,14 @@ class Service
         <input type="text" name="company_slogan" id="company_slogan"
                value="<?php echo get_option('company_slogan'); ?>"/>
         <?php
+    }
+
+    public function company_tax_id_form_element(){
+        ?>
+        <input type="text" name="company_tax_id" id="company_tax_id"
+               value="<?php echo get_option('company_tax_id'); ?>"/>
+        <?php
+
     }
 
 
